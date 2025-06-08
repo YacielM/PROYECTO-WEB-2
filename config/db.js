51709e32da4a4af2fada194f649c1ca506700f68
YacielM <yacielzombers@gmail.com>
2025-06-07 21:39:51 -0300

    tercer y ultimo intento

--- config/db.js
+++ config/db.js
@@ -1,5 +1,6 @@
 // db.js
 const { Sequelize } = require('sequelize');
+const mysql2 = require('mysql2'); // Agrega esta línea
 
 const sequelize = new Sequelize(
   'his_db',   // Nombre de tu BD en MySQL Workbench
@@ -8,6 +9,7 @@ const sequelize = new Sequelize(
   {
     host: 'localhost',
     dialect: 'mysql',
+    dialectModule: mysql2, // Agrega esta opción para forzar el uso de mysql2
     logging: false // Oculta los logs de SQL en consola
   }
 );
